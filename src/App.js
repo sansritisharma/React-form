bac2dda16c32158b19da7c6eae95ce6e7a7a01ac
sansritisharma <87527606+sansritisharma@users.noreply.github.com>
2024-04-12 08:21:44 +0530

    with useSate object and prev state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [formData, setFormData] = useState({
+    name: "",
+    mail: "",
+    password: "",
+  });
+
+  const inputEvent = (e) => {
+    const val = e.target.value;
+    const name = e.target.name;
+
+    setFormData((prev) => {
+      if (name === "name") {
+        return {
+          name: val,
+          mail: prev.mail,
+          password: prev.password,
+        };
+      } else if (name === "mail") {
+        return {
+          name: prev.name,
+          mail: val,
+          password: prev.password,
+        };
+      } else if (name === "password") {
+        return {
+          name: prev.name,
+          mail: prev.mail,
+          password: val,
+        };
+      }
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <form onSubmit={handleSubmit}>
+        <h1>Hi {formData.name}</h1>
+        <input
+          type="text"
+          placeholder="Enter your name"
+          name="name"
+          onChange={inputEvent}
+        />
+
+        <input
+          type="text"
+          name="mail"
+          placeholder="Enter your mail"
+          onChange={inputEvent}
+        />
+
+        <input
+          type="text"
+          name="password"
+          placeholder="Enter your password"
+          onChange={inputEvent}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <div
+        style={{
+          border: "1px solid  rgb(124, 236, 141)",
+          padding: "10px 20px",
+          marginTop: "20px",
+        }}
+      >
+        <h2>Form Data :</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {formData.name} {formData.mail} {formData.password}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
